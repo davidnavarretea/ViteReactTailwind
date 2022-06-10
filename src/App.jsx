995972952ac58ce50521f5591ea5b1b1d23a73c2
David <davidnavarretea1996@gmail.com>
2022-06-10 04:04:49 +0200

    ViteReactTailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+function App() {
+  /* useStages, useEffects, functions */
+  return (
+    <div className='container mx-auto mt-9'>
+      {/* Componentes */}
+    </div>
+  )
+}
+
+export default App
